e053d1ce1cdee5a2f985b2c54a170ff5aca95e27
Johann Deneux <johann.deneux@gmail.com>
2016-02-01 10:22:26 +0100

    Added copyright notice and license terms.

--- projects/InjectPrio.Cli/Program.fs
+++ projects/InjectPrio.Cli/Program.fs
@@ -1,5 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿// InjectPrio.Cli Copyright (C) 2016 Johann Deneux <johann.deneux@gmail.com>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 open InjectPrio.Build
 
