cefcbf3d3d10bfdedb3c2db9a7b036d2f434805b
Johann Deneux <johann.deneux@gmail.com>
2016-02-02 19:22:59 +0100

    Added 2nd wave of patrols and IL-2s, to allow for a longer mission time (3h). This is needed for the slow tanks to reach their destination.

--- projects/TickDelayMonitoring/BuildEarlyInvasion.fsx
+++ projects/TickDelayMonitoring/BuildEarlyInvasion.fsx
@@ -11,13 +11,14 @@ open InjectPrio.Build
 // Names of MCUs that control starting and stopping vehicle groups
 let instances =
     [
-        for i in 1..3 do
+        for i in 1..6 do
             yield UnitPrioApi.Create(sprintf "KillBf109-%d" i, setHighPrio = sprintf "HiPrioBf109-%d" i, setMedPrio = sprintf "MedPrioBf109-%d" i)
             yield UnitPrioApi.Create(sprintf "KillLagg3-%d" i, setHighPrio = sprintf "HiPrioLagg3-%d" i, setMedPrio = sprintf "MedPrioLagg3-%d" i)
+        for i in 1..3 do
             yield UnitPrioApi.Create(sprintf "KillPz3-%d" i, setLowPrio = sprintf "StartPz3-%d" i)
         for i in 1..5 do
             yield UnitPrioApi.Create(sprintf "KillBf110-%d" i, setHighPrio = sprintf "StartBf110-%d" i, setMedPrio = sprintf "LowPrioBf110-%d" i)
-        for i in 1..2 do
+        for i in 1..4 do
             yield UnitPrioApi.Create(sprintf "KillIL2-%d" i, setHighPrio = sprintf "StartIL2-%d" i, setMedPrio = sprintf "LowPrioIL2-%d" i)
         for i in 1..3 do
             yield UnitPrioApi.Create(sprintf "KillAfAAA-%d" i, setMedPrio = sprintf "StartAfAAA-%d" i, setLowPrio = sprintf "LowPrioAfAAA-%d" i)
