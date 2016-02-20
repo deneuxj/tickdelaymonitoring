2159ab72ef34d8945c669d465f1b10848e017eaa
Johann Deneux <johann.deneux@gmail.com>
2016-02-20 10:56:14 +0100

    Moved RCon client code to a separate project.

---
+++ projects/RConClient/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace RConClient.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("RConClient")>]
+[<assembly: AssemblyDescription("Remote console client library for IL-2 Sturmovik's DServer")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("RConClient")>]
+[<assembly: AssemblyCopyright("Copyright © Johann Deneux 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5928d2af-c716-4690-9718-aa84ae54c3d6")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ projects/RConClient/Client.fs
@@ -0,0 +1,142 @@
+﻿// RConClient Copyright (C) 2016 Johann Deneux <johann.deneux@gmail.com>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace RConClient
+
+open System.Net
+open System.Text
+open System.Web
+open System
+
+type StepPerSecData =
+    { Current : float32
+      Minimum : float32
+      Maximum : float32
+      Average : float32
+    }
+
+exception ConnectionException of unit
+
+/// <summary>
+/// Interaction between the web server and DServer.
+/// Provides authentication, triggering server inputs...
+/// </summary>
+type Client(address : IPAddress, port, login, password) as this =
+    inherit Sockets.TcpClient()
+
+    do
+        try
+            base.Connect(address, port)
+        with
+        | exc ->
+            printfn "Failed to connect to game server: %s" exc.Message
+            raise(ConnectionException())
+    let stream = this.GetStream()
+
+    let send(buff, idx, len) = Async.FromBeginEnd((fun (cb, par) -> stream.BeginWrite(buff, idx, len, cb, par)), stream.EndWrite)
+    let receive(buff, idx, len) = Async.FromBeginEnd((fun (cb, par) -> stream.BeginRead(buff, idx, len, cb, par)), stream.EndRead)
+
+    let encode (cmd : string) =
+        let asAscii = Encoding.ASCII.GetBytes(cmd)
+        let length = [| byte((asAscii.Length + 1) % 256) ; byte((asAscii.Length + 1) / 256) |]
+        let buff = Array.concat [length ; asAscii ; [|0uy|] ]
+        buff
+
+    let decodeResponse(response : string) =
+        [
+            for pair in response.Split('&') do
+                match pair.Split('=') with
+                | [| key; value |] -> yield (key, value)
+                | _ -> failwithf "Could not split key-value pair %s" pair
+        ]
+
+    let getResponse(stream : Sockets.NetworkStream) =
+        async {
+            let buffer : byte[] = Array.zeroCreate 0xffff
+            let response = stream.Read(buffer, 0, 2)
+            let responseLength = (256 * int buffer.[1]) + int buffer.[0]
+            let! response = receive(buffer, 2, responseLength)
+            let data =
+                if responseLength > 0 then
+                    Encoding.ASCII.GetString(buffer, 2, responseLength - 1)
+                else
+                    ""
+            return data
+        }
+
+    let parseFloat s =
+        System.Single.Parse(s, System.Globalization.CultureInfo.InvariantCulture)
+
+    member this.Auth() =
+        async {
+            let buff = encode <| sprintf "auth %s %s" login password
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            return response
+        }
+
+    member this.ServerInput(name) =
+        async {
+            let buff =
+                sprintf "serverinput %s" name
+                |> encode
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            return response
+        }
+
+    member this.MessagePlayer(playerId, msg) =
+        async {
+            let buff =
+                sprintf "chatmsg 3 %d %s" playerId msg
+                |> encode
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            return response
+        }
+
+    member this.MessageTeam(teamId, msg) =
+        async {
+            let buff =
+                sprintf "chatmsg 1 %d %s" teamId msg
+                |> encode
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            return response
+        }
+
+    member this.GetSPS() =
+        async {
+            let buff = encode "spsget"
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            let decoded =
+                decodeResponse response
+                |> dict
+            return
+                { Current = decoded.["curSps"] |> parseFloat
+                  Minimum = decoded.["minSps"] |> parseFloat
+                  Maximum = decoded.["maxSps"] |> parseFloat
+                  Average = decoded.["avgSps"] |> parseFloat
+                }
+        }
+
+    member this.ResetSPS() =
+        async {
+            let buff = encode "spsreset"
+            do! send(buff, 0, buff.Length)
+            let! response = getResponse stream
+            return response
+        }
