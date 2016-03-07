ba0b813f922d0dac7baf8843291d7e6c67eea0b6
Johann Deneux <johann.deneux@gmail.com>
2016-03-07 17:01:33 +0100

    New version 1.2.0.0

--- projects/RConClient/AssemblyInfo.fs
+++ projects/RConClient/AssemblyInfo.fs
@@ -4,6 +4,10 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
+module Constants =
+    [<Literal>]
+    let version = "1.2.0.0"
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -34,8 +38,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.1.0.0")>]
-[<assembly: AssemblyFileVersion("1.1.0.0")>]
-
+[<assembly: AssemblyVersion(Constants.version)>]
+[<assembly: AssemblyFileVersion(Constants.version)>]
 do
     ()
